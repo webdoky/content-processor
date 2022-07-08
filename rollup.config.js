6a1710611d967e8a6b6e8b5d2ae7c089114f4ecd
Mykola Myslovskyi <adriandecita@gmail.com>
2022-07-08 11:50:57 +0300

    feat: restore rollup confing with new setup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { importAssertionsPlugin } from 'rollup-plugin-import-assert';
+import { importAssertions } from 'acorn-import-assertions';
+
+export default {
+  input: './build/src/main.js',
+  output: {
+    dir: 'dist',
+    format: 'es',
+  },
+  acornInjectPlugins: [ importAssertions ],
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      preferBuiltins: true,
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    json(),
+    importAssertionsPlugin(),
+  ],
+  external: [
+    'mdast-util-to-hast',
+    'glob',
+    'gray-matter',
+    'unified',
+    'rehype-prism',
+    'rehype-parse',
+    'rehype-autolink-headings',
+    'rehype-raw',
+    'lodash',
+    'github-slugger',
+    'cyrillic-to-translit-js',
+    'rimraf',
+    "mdast-util-to-string",
+    "prism-themes",
+    "rehype-external-links",
+    "rehype-stringify",
+    "remark-gfm",
+    "remark-parse",
+    "remark-rehype",
+    "unist-util-visit",
+    "unist-util-visit-parents",
+  ],
+};
