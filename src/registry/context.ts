2741e34ea5e4025d872ea1eb29598c0821bc2975
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2022-10-29 01:02:41 +0300

    feat: this.web.getJSONData helper

--- src/registry/context.ts
+++ src/registry/context.ts
@@ -1,5 +1,4 @@
 import type Registry from '.';
-import PageService from './environment/page';
 import WebService from './environment/web';
 
 export interface Environment {
@@ -12,14 +11,12 @@ export interface Environment {
 
 export default class Context {
   env: Environment;
-  page: PageService;
   registry: Registry;
   web: WebService;
 
   constructor(env: Environment, registry: Registry) {
     this.env = env;
-    this.page = new PageService(this);
     this.registry = registry;
-    this.web = new WebService();
+    this.web = new WebService(this);
   }
 }
--- src/registry/environment/web.ts
+++ src/registry/environment/web.ts
@@ -1,7 +1,31 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import type Context from '../context';
+
 import escapeHtml from '../utils/escape-html';
 import convertSpacesToUnderscores from '../utils/spaces-to-underscores';
 
 export default class WebService {
+  context: Context;
+  constructor(context: Context) {
+    this.context = context;
+  }
+  /**
+   * Return specific .json files from the content root
+   */
+  getJSONData(name: string): unknown {
+    const filePath = path.join(
+      this.context.registry._options.pathToOriginalContent,
+      'jsondata',
+      `${name}.json`,
+    );
+    try {
+      return readJSONDataFile(filePath);
+    } catch (error) {
+      console.error(`Tried to read JSON from ${filePath}`, error);
+      throw error;
+    }
+  }
   /**
    * Creates a link HTML
    */
@@ -16,4 +40,16 @@ export default class WebService {
     out.push('>', escapeHtml(text || uri), '</a>');
     return out.join('');
   }
+}
+
+const _readJSONDataCache = new Map<string, unknown>();
+function readJSONDataFile(filePath: string): unknown {
+  if (_readJSONDataCache.has(filePath)) {
+    return _readJSONDataCache.get(filePath);
+  }
+  const payload = JSON.parse(readFileSync(filePath, 'utf-8'));
+  if (process.env.NODE_ENV === 'production') {
+    _readJSONDataCache.set(filePath, payload);
+  }
+  return payload;
 }
